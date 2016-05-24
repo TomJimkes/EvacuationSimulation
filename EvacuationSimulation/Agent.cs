bbd30b102d4b0e3c04cb228f7870a0fd9463f055
Tom Jimkes <tomjimkes@gmail.com>
2016-05-24 13:39:06 +0200

    Added several classes

---
+++ EvacuationSimulation/Agent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvacuationSimulation
+{
+    class Agent
+    {
+        private object physicalState;
+        private object mentalState;
+        private Floor mentalMap;
+    }
+}

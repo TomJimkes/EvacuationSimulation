9207776b3a5f88ccd6c1e8d972e3c9ca1093cb48
Jasper Kraimaat <jasper.kraimaat@gmail.com>
2016-05-26 11:36:48 +0200

    Move scripts to the correct location

--- EvacuationSimulation/Assets/Scripts/GridSquare.cs
+++ EvacuationSimulation/Assets/Scripts/GridSquare.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace EvacuationSimulation
 {
-    class FloorGraph
+    class GridSquare
     {
     }
 }

51916722ca47ee1262b39705f231e24d1d8060a3
Jasper Kraimaat <jasper.kraimaat@gmail.com>
2016-05-26 13:00:50 +0200

    Added basic tile drawing

--- EvacuationSimulation/Assets/Scripts/FloorGrid.cs
+++ EvacuationSimulation/Assets/Scripts/FloorGrid.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
+﻿using UnityEngine;
 
 namespace EvacuationSimulation
 {
-    class FloorGrid
+    class FloorGrid : MonoBehaviour
     {
+        GridSquare[,] grid = new GridSquare[5,5];
+
+        void Start()
+        {
+            GameObject tile = Resources.Load<GameObject>("Prefabs/Floortile");
+            for (int x = 0; x < grid.GetLength(0); x++)
+            {
+                for (int y = 0; y < grid.GetLength(0); y++)
+                {
+                    var t = Instantiate(tile);
+                    t.transform.localPosition = new Vector3(x, y);
+                    t.transform.parent = transform;
+                }
+            }
+        }
     }
 }

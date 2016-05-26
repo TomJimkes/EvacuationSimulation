994e288a9262565f85d0ce9b775bb17f036fee79
Jasper Kraimaat <jasper.kraimaat@gmail.com>
2016-05-26 14:31:09 +0200

    Add wall tiles

--- EvacuationSimulation/Assets/Scripts/FloorGrid.cs
+++ EvacuationSimulation/Assets/Scripts/FloorGrid.cs
@@ -2,18 +2,29 @@
 
 namespace EvacuationSimulation
 {
+    enum TileType
+    { Empty, Wall, Other }
+
     class FloorGrid : MonoBehaviour
     {
-        bool[,] grid = new bool[5,5];
+        TileType[,] grid = new TileType[5, 5];
 
         void Start()
         {
-            GameObject tile = Resources.Load<GameObject>("Prefabs/Floortile");
+            GameObject[] tilePrefabs = new GameObject[3];
+            tilePrefabs[0] = Resources.Load<GameObject>("Prefabs/Floortile");
+            tilePrefabs[1] = Resources.Load<GameObject>("Prefabs/Walltile");
+            tilePrefabs[2] = tilePrefabs[0];
+
+            grid[2,2] = TileType.Wall;
+            grid[2,3] = TileType.Wall;
+            grid[2,4] = TileType.Wall;
+
             for (int x = 0; x < grid.GetLength(0); x++)
             {
                 for (int y = 0; y < grid.GetLength(0); y++)
                 {
-                    var t = Instantiate(tile);
+                    var t = Instantiate(tilePrefabs[(int)grid[x, y]]);
                     t.transform.localPosition = new Vector3(x, y);
                     t.transform.parent = transform;
                 }

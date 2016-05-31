f57f6ad5b1117e58b950df03b8d823b2502af4ea
Jasper Kraimaat <jasper.kraimaat@gmail.com>
2016-05-31 12:58:59 +0200

    Load fire sprites from Unity

--- EvacuationSimulation/Assets/Scripts/FireGrid.cs
+++ EvacuationSimulation/Assets/Scripts/FireGrid.cs
@@ -5,21 +5,15 @@ namespace EvacuationSimulation
     class FireGrid : MonoBehaviour
     {
         GameObject[,] fireObjects = new GameObject[5,5];
-        Sprite[] fireSprites = new Sprite[3];
         GameObject firePrefab;
 
         void Start()
         {
-            fireSprites[0] = Resources.Load<Sprite>("Sprites/Fire1");
-            fireSprites[1] = Resources.Load<Sprite>("Sprites/Fire2");
-            fireSprites[2] = Resources.Load<Sprite>("Sprites/Fire3");
-
             firePrefab = Resources.Load<GameObject>("Prefabs/Fire");
             var f = Instantiate(firePrefab);
             f.transform.localPosition = new Vector3(2, 2);
             f.transform.parent = transform;
             fireObjects[2, 2] = f;
-            f.GetComponent<Fire>().fireSprites = fireSprites;
         }
 
         void Update()
